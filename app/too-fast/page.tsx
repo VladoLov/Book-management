38223711312d5d0ac5553640a10e647105cfe7df
VladoLov <vladislav.lovric@gmail.com>
2025-01-18 18:07:33 +0100

    add upstah

---
+++ app/too-fast/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <main className="root-container flex min-h-screen flex-col items-center justify-center">
+      <h1 className="font-bebas-neue text-5xl font-bold text-light-100">
+        Slow down
+      </h1>
+      <p className="mt-3 max-w-3xl text-center text-light-400">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse quam
+        itaque sint numquam et praesentium perferendis doloribus. Voluptatum,
+        ipsa nemo!
+      </p>
+    </main>
+  );
+}
---
+++ lib/ratelimit.ts
@@ -0,0 +1,20 @@
+import redis from "@/db/redis";
+import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
+
+// Create a new ratelimiter, that allows 10 requests per 10 seconds
+const ratelimit = new Ratelimit({
+  redis: redis,
+  limiter: Ratelimit.slidingWindow(5, "1m"),
+  analytics: true,
+  /**
+   * Optional prefix for the keys used in redis. This is useful if you want to share a redis
+   * instance with other applications and want to avoid key collisions. The default prefix is
+   * "@upstash/ratelimit"
+   */
+  prefix: "@upstash/ratelimit",
+});
+
+// Use a constant string to limit all requests with a single ratelimit
+// Or use a userID, apiKey or ip address for individual limits.
+
+export default ratelimit;
