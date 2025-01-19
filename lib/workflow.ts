9416256af8017c569243099401a81bcef47eed07
VladoLov <vladislav.lovric@gmail.com>
2025-01-19 15:18:03 +0100

    adding workflow

--- lib/workflow.ts
+++ lib/workflow.ts
@@ -1,7 +1,34 @@
 import { Client as WorkflowClient } from "@upstash/workflow";
+import { Client as QstashClient, resend } from "@upstash/qstash";
 import config from "./config";
 
 export const workflowClient = new WorkflowClient({
   baseUrl: config.env.upstash.qstashUrl,
   token: config.env.upstash.qstashToken,
 });
+const qstashClient = new QstashClient({
+  token: config.env.upstash.qstashToken,
+});
+
+export const sendEmail = async ({
+  email,
+  subject,
+  message,
+}: {
+  email: string;
+  subject: string;
+  message: string;
+}) => {
+  await qstashClient.publishJSON({
+    api: {
+      name: "email",
+      provider: resend({ token: config.env.resendToken }),
+    },
+    body: {
+      from: "Acme <onboarding@resend.dev>", //here we add register domain on resend
+      to: email, // here we add email to send
+      subject: subject,
+      html: message,
+    },
+  });
+};
